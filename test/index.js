619e2a79439a0e2eb43d53c6c7a497fe290e17cc
Felix Gnass <fgnass@gmail.com>
2014-09-26 21:47:59 +0200

    add innerHTML support

--- test/index.js
+++ test/index.js
@@ -1,45 +1,84 @@
 var dommy = require('..')
-  , assert = require('assert')
+  , test = require('tape')
 
-var doc = dommy()
-assert.equal(doc.outerHTML, '<!DOCTYPE html>\n')
 
-var el = doc.createElement('div')
+test('document', function(t) {
+  var doc = dommy()
+  t.equal(doc.outerHTML, '<!DOCTYPE html>\n')
+  t.end()
+})
 
-assert.equal(el.outerHTML, '<div></div>')
-assert.equal(el.ownerDocument, doc)
+test('element', function(t) {
+  var doc = dommy()
+  var el = doc.createElement('div')
 
-var a = doc.createElement('a')
-a.setAttribute('href', 'foo')
+  t.equal(el.outerHTML, '<div></div>', 'div is empty')
+  t.equal(el.ownerDocument, doc, 'ownerDocument is set')
 
-assert.equal(a.outerHTML, '<a href="foo"></a>')
+  el.appendChild(doc.createTextNode('Hello <3'))
+  t.equal(el.innerHTML, 'Hello &lt;3', 'innerHTML is escaped')
 
-a.appendChild(doc.createTextNode('Hello'))
+  t.end()
+})
 
-assert.equal(a.innerHTML, 'Hello')
-assert.equal(a.outerHTML, '<a href="foo">Hello</a>')
+test('attributes', function(t) {
+  var doc = dommy()
+  var a = doc.createElement('a')
+  a.setAttribute('href', 'foo')
+  t.equal(a.outerHTML, '<a href="foo"></a>', 'attribute is serialized')
+  t.end()
+})
 
-el.appendChild(a)
-assert.equal(el.outerHTML, '<div><a href="foo">Hello</a></div>')
+test('classes', function(t) {
+  var doc = dommy()
+  var el = doc.createElement('div')
+  el.className = 'foo'
 
-el = doc.createElement('div')
-el.className = 'foo'
+  t.equal(el.getAttribute('class'), 'foo', 'class attribute reflects className')
 
-assert.equal(el.getAttribute('class'), 'foo')
-assert.equal(el.outerHTML, '<div class="foo"></div>')
+  t.equal(el.outerHTML, '<div class="foo"></div>', 'class attribute is serialized')
 
-assert.ok(el.classList.contains('foo'))
+  t.ok(el.classList.contains('foo'), 'classList reflects class set via class attribute')
 
-el.classList.add('bar')
-assert.equal(el.className, 'foo bar')
+  el.classList.add('bar')
+  t.equal(el.className, 'foo bar', 'added class')
 
-el.classList.add('bar')
-assert.equal(el.className, 'foo bar')
+  el.classList.add('bar')
+  t.equal(el.className, 'foo bar', 'not added twice')
 
-el.classList.toggle('foo')
-assert.equal(el.className, 'bar')
+  el.classList.toggle('foo')
+  t.equal(el.className, 'bar', 'remove class through toggle')
 
-el.className = ''
-assert.equal(el.outerHTML, '<div></div>')
+  el.className = ''
+  t.equal(el.outerHTML, '<div></div>', 'empty className removes class attribute')
+  t.end()
+})
 
-console.log('All tests passed.')
+test('innerHTML', function(t) {
+  var doc = dommy()
+  var el = doc.createElement('div')
+  el.appendChild(doc.createTextNode('foo'))
+  t.equal(el.outerHTML, '<div>foo</div>', 'serialized html contains childNodes')
+  el.innerHTML = ''
+  t.equal(el.outerHTML, '<div></div>', 'clearing innerHTML removes all children')
+
+  el.innerHTML = '<h1>hello</h1>'
+  t.equal(el.outerHTML, '<div><h1>hello</h1></div>', 'serialized html reflects innerHTML')
+  t.end()
+})
+
+test('fragments', function(t) {
+  var doc = dommy()
+  var f = doc.createDocumentFragment()
+  f.appendChild(doc.createTextNode('foo'))
+  t.equal(f.innerHTML, 'foo')
+
+  f.innerHTML = '<h1>hello</h1>'
+  t.equal(f.toString(), '<h1>hello</h1>')
+
+  var el = doc.createElement('div')
+  el.appendChild(f)
+
+  t.equal(el.outerHTML, '<div><h1>hello</h1></div>')
+  t.end()
+})
